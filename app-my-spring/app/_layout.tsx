a5b08ee680814e58912916e8165db9a060daca08
Kosta-111 <nestt@ukr.net>
2025-04-12 19:45:51 +0300

    Add register and login tabs

--- app-my-spring/app/_layout.tsx
+++ app-my-spring/app/_layout.tsx
@@ -5,7 +5,8 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-import "../global.css";
+
+import "../global.css"
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -31,7 +32,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
