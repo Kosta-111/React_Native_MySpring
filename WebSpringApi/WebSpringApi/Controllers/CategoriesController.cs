e728f27a51a635f55571b13639c473715c4d223c
Kosta-111 <nestt@ukr.net>
2025-05-16 21:56:41 +0300

    Categories CRUD backend

--- WebSpringApi/WebSpringApi/Controllers/CategoriesController.cs
+++ WebSpringApi/WebSpringApi/Controllers/CategoriesController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Authorization;
 using WebSpringApi.Data;
 using WebSpringApi.Models.Category;
 using WebSpringApi.Data.Entities.Identity;
+using WebSpringApi.Abstract;
+using WebSpringApi.Data.Entities;
 
 namespace WebSpringApi.Controllers;
 
@@ -14,6 +16,7 @@ namespace WebSpringApi.Controllers;
 [Route("api/[controller]")]
 public class CategoriesController(
     IMapper mapper,
+    IImageService imageService,
     WebSpringDbContext context,
     UserManager<UserEntity> userManager
     ) : ControllerBase
@@ -23,14 +26,11 @@ public class CategoriesController(
     {
         try
         {
-            var userEmail = User.Claims.FirstOrDefault()?.Value 
-                ?? throw new Exception("user email undefined");
-
-            var user = await userManager.FindByEmailAsync(userEmail)
-                ?? throw new Exception($"user by email {userEmail} not found");
+            var userId = await GetUserId()
+                ?? throw new Exception("user not found!");
 
             var categoriesList = context.Categories
-                .Where(x => x.UserId == user.Id)
+                .Where(x => x.UserId == userId)
                 .ProjectTo<CategoryItemViewModel>(mapper.ConfigurationProvider)
                 .ToList();
             
@@ -40,5 +40,68 @@ public class CategoriesController(
         {
             return BadRequest(new { error = ex.Message });
         }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromForm] CategoryCreateViewModel model)
+    {
+        var userId = await GetUserId();
+
+        if (userId is null) 
+            return BadRequest(new { error = "user not found!" });
+
+        var category = mapper.Map<CategoryEntity>(model);
+        category.Image = await imageService.SaveImageAsync(model.Image);
+        category.UserId = userId.Value;
+
+        context.Categories.Add(category);
+        context.SaveChanges();
+        return Ok(new { id = category.Id });
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Edit([FromForm] CategoryEditViewModel model)
+    {
+        var category = context.Categories.SingleOrDefault(x => x.Id == model.Id);
+        
+        if (category is null) return NotFound();
+
+        category = mapper.Map(model, category);
+        
+        if (model.Image is not null)
+        {
+            string? deleteImage = category.Image;
+            category.Image = await imageService.SaveImageAsync(model.Image);
+            
+            if (deleteImage is not null)
+                imageService.DeleteImageIfExists(deleteImage);
+        }
+        context.SaveChanges();
+        return Ok(new { id = category.Id });
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Remove(int id)
+    {
+        var category = context.Categories.SingleOrDefault(x => x.Id == id);
+        
+        if (category is null) return NotFound();
+
+        if (category.Image is not null)
+        {
+            imageService.DeleteImageIfExists(category.Image);
+        }
+        context.Categories.Remove(category);
+        context.SaveChanges();
+        return Ok();
+    }
+
+    private async Task<long?> GetUserId()
+    {
+        var userEmail = User.Claims.FirstOrDefault()?.Value;        
+        if (userEmail is null) return null;
+
+        var user = await userManager.FindByEmailAsync(userEmail);
+        return user?.Id;
     }
 }
